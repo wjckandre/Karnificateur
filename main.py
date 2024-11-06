339c058a5a614532acd91a6b89ab7d5c693ca3a9
AlbertVador <albert.vador@gmail.com>
2024-11-06 10:42:05 -0500

    aff

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print('hello')
+def affK(table):
+    maxL = max([len(x) for x in table[0]+table[1]])
+    printable = ""
+    for i in range(len(table[0])):
+        table[0][i] = table[0][i].ljust(maxL)
+    for i in range(len(table[1])):
+        table[1][i] = table[1][i].ljust(maxL)
+    printable += "|"+"|".join(table[0]+table[1])+"|\n"
+    for ligne in table[2:]:
+        printable+= "|"+"|".join([v.ljust(maxL) for v in ligne[0]+ligne[1]])+"|\n"
+    print(printable)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+table = [
+    ["a0","a1","b"], # nom entrees
+    ["x","s"], # nom sorties
+    ["000","01"], # ligne 1
+    ["001","11"]  # ligne 2
+]
+
+affK(table)
