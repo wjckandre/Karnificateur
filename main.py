49e317b86a787003e14fa70a6542ad60cc8b6b54
AlbertVador <albert.vador@gmail.com>
2024-11-06 10:45:13 -0500

    blablabla

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ def table_verite():
     return table
 
 print(table_verite())
+
+def affK(table):
+    maxL = max([len(x) for x in table[0]+table[1]])
+    printable = ""
+    for i in range(len(table[0])):
+        table[0][i] = table[0][i].ljust(maxL)
+    for i in range(len(table[1])):
+        table[1][i] = table[1][i].ljust(maxL)
+    printable += "|"+"|".join(table[0]+table[1])+"|\n"
+    for ligne in table[2:]:
+        printable+= "|"+"|".join([v.ljust(maxL) for v in ligne[0]+ligne[1]])+"|\n"
+    print(printable)
