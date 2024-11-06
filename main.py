5094c9e76f7734f1a5eee141dc238b4c9e7b08db
wjckandre <wojcik.andre2.0@gmail.com>
2024-11-06 10:35:37 -0500

    rentrée de données faite

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hello')
+
+def table_verite():
+    table = []
+    nb_entry = int(input("nb_entry : "))
+    nb_sortie = int(input("nb_sortie : "))
+    temp = []
+    for k in range(nb_entry):
+        temp.append(input(f"nom de la {k}ième entrées : "))
+    table.append(temp)
+    temp = []
+    for i in range(nb_sortie):
+        temp.append(input(f"nom de la {i}ième sorties : "))
+    table.append(temp)
+    for x in range(pow(2,nb_entry)):
+        table.append([input("entrées : "), input("sorties : ")])
+    return table
+
+print(table_verite())
