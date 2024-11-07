0cac1cb4dd485eee1c1270d1ddc3373349942a7e
AlbertVador <albert.vador@gmail.com>
2024-11-06 22:46:58 -0500

    oups correction

--- main.py
+++ main.py
@@ -40,11 +40,11 @@ def affK(table):
     table,maxL = format(table)
     printable = ""
     mid = len(table[0])//2+len(table[0])%2
-    printable += "|"+"".join(table[0][:mid])+"|"+"".join(table[0][mid:]) + " " + " ".join([bin(gray(bin(i)[2:]))[2:].zfill(len(table[0][:mid])) for i in range(pow(2,len(table[0][mid:])))]) + "|\n"
     kValues1 = [bin(gray(bin(i)[2:]))[2:].zfill(len(table[0][:mid])) for i in range(pow(2,len(table[0][:mid])))]
     kValues2 = [bin(gray(bin(i)[2:]))[2:].zfill(len(table[0][:mid])) for i in range(pow(2,len(table[0][mid:])))]
     for s in range(len(table[1])):
         printable += f"Sortie {table[1][s]} :\n"
+        printable += "|"+"".join(table[0][:mid])+"|"+"".join(table[0][mid:]) + " " + " ".join([bin(gray(bin(i)[2:]))[2:].zfill(len(table[0][:mid])) for i in range(pow(2,len(table[0][mid:])))]) + "|\n"
         for kv in kValues1:
             printable += "|" + "".join([v.ljust(maxL) for v in list(kv)]) + "|" + " "*(maxL*len(table[0][mid:])+1) + (" "*maxL).join([table[int(kv+x,2)+2][1][s] for x in kValues2]) + " |\n"
     print(printable)
