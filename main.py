8cd8a56fdf8ed10747e0fcff3515ce63622987e6
AlbertVador <albert.vador@gmail.com>
2024-11-06 23:30:46 -0500

    yay fini

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-
 def table_verite():
     table = []
-    nb_entry = int(input("nb_entry : "))
-    nb_sortie = int(input("nb_sortie : "))
+    nb_entrees = int(input("nombre d'entrées : "))
+    nb_sorties = int(input("nombre de sorties : "))
     temp = []
-    for k in range(nb_entry):
-        temp.append(input(f"nom de la {k+1} entrées : "))
+    for k in range(nb_entrees):
+        temp.append(input(f"nom de l'entrée {k+1} : "))
     table.append(temp)
     temp = []
-    for i in range(nb_sortie):
-        temp.append(input(f"nom de la {i+1} sorties : "))
+    for i in range(nb_sorties):
+        temp.append(input(f"nom de la sortie {i+1} : "))
     table.append(temp)
-    for x in range(pow(2,nb_entry)):
-        table.append([input("entrées : "), input("sorties : ")])
+    for x in range(2**nb_entrees):
+        vEntree = bin(x)[2:].zfill(len(table[0]))
+        table.append([vEntree,"".join([input(f"{table[1][z]} pour {vEntree} : ") for z in range(nb_sorties)])])
     return table
 
 def gray(b):
@@ -44,14 +44,14 @@ def affK(table):
     kValues2 = [bin(gray(bin(i)[2:]))[2:].zfill(len(table[0][mid:])) for i in range(pow(2,len(table[0][mid:])))]
     for s in range(len(table[1])):
         printable += f"Sortie {table[1][s]} :\n"
-        printable += "|"+"".join(table[0][:mid])+"|"+"".join(table[0][mid:]) + " " + " ".join([bin(gray(bin(i)[2:]))[2:].zfill(len(table[0][:mid])) for i in range(pow(2,len(table[0][mid:])))]) + "|\n"
+        printable += "|"+"".join(table[0][:mid])+"|"+"".join(table[0][mid:]) + " " + " ".join([bin(gray(bin(i)[2:]))[2:].zfill(len(table[0][mid:])) for i in range(pow(2,len(table[0][mid:])))]) + "|\n"
         for kv in kValues1:
-            printable += "|" + "".join([v.ljust(maxL) for v in list(kv)]) + "|" + " "*(maxL*len(table[0][mid:])+1) + (" "*maxL).join([table[int(kv+x,2)+2][1][s] for x in kValues2]) + " |\n"
+            printable += "|" + "".join([v.ljust(maxL) for v in list(kv)]) + "|" + " "*(maxL*len(table[0][mid:])+1) + (" "*len(kValues2[0])).join([table[int(kv+x,2)+2][1][s] for x in kValues2]) + " "*(len(table[0][mid:])-1) + "|\n"
     print(printable)
 
 
 table = [
-    ["a1","a0","b1","b0"],
+    ["a1","a0","a3","a2"],
     ["s","x"],
     ["0000","10"],
     ["0001","01"],
@@ -70,4 +70,6 @@ table = [
     ["1110","00"],
     ["1111","11"]
 ]
-affK(table)
+t = table_verite()
+affTV(t)
+affK(t)
