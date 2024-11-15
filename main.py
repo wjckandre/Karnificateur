bfe4ea8e84fa01ad829eb2347775b9dabd6ee9d1
AlbertVador <albert.vador@gmail.com>
2024-11-14 22:31:57 -0500

    tkinter

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 def table_verite():
     table = []
     nb_entrees = int(input("nombre d'entrées : "))
@@ -49,6 +51,53 @@ def affK(table):
             printable += "|" + "".join([v.ljust(maxL) for v in list(kv)]) + "|" + " "*(maxL*len(table[0][mid:])+1) + (" "*len(kValues2[0])).join([table[int(kv+x,2)+2][1][s] for x in kValues2]) + " "*(len(table[0][mid:])-1) + "|\n"
     print(printable)
 
+def affTVtk(table):
+    root = Tk()
+    table[0] = table[0]+table.pop(1)
+    print(table)
+    table = [table[0]] + [list(x[0])+list(x[1]) for x in table[1:]]
+    print(table)
+    for i in range(len(table)):
+        for j in range(len(table[0])):
+            c = Label(root, text=table[i][j])
+            c.grid(row=i, column=j)
+    mainloop()
+
+def affKtk(table):
+    root = Tk()
+    offset=0
+    mid = len(table[0])//2+len(table[0])%2
+    entries1 = table[0][:mid]
+    entries2 = table[0][mid:]
+    sorties = table[1]
+    table = [table[:2]] + [list(x[1]) for x in table[2:]]
+    print(table)
+    for s in range(len(sorties)):
+        c = Label(root, text=f"Sortie '{sorties[s]}'")
+        c.grid(row=0, column=0+offset, columnspan=10)
+        c = Label(root, text="".join(entries1))
+        c.grid(row=1, column=0+offset)
+        c = Label(root, text="|")
+        c.grid(row=1, column=1+offset)
+        c = Label(root, text="".join(entries2))
+        c.grid(row=1, column=2+offset)
+        kValues1 = [bin(gray(bin(i)[2:]))[2:].zfill(len(entries1)) for i in range(pow(2,len(entries1)))]
+        kValues2 = [bin(gray(bin(i)[2:]))[2:].zfill(len(entries2)) for i in range(pow(2,len(entries2)))]
+        for kv in kValues2:
+            c = Label(root, text=kv)
+            c.grid(row=1, column=3+kValues2.index(kv)+offset)
+        for kv in kValues1:
+            c = Label(root, text=kv)
+            c.grid(row=kValues1.index(kv)+2, column=0+offset)
+            c = Label(root, text="|")
+            c.grid(row=kValues1.index(kv)+2, column=1+offset)
+            for x in range(len(kValues2)):
+                c = Label(root, text=table[int(kv+kValues2[x],2)+1][s])
+                c.grid(row=kValues1.index(kv)+2, column=3+x+offset)
+        c = Label(root, text="    ")
+        c.grid(row=1, column=4+x+offset)
+        offset+=3+len(kValues2)+1
+    mainloop()
 
 table = [
     ["a1","a0","a3","a2"],
@@ -70,6 +119,10 @@ table = [
     ["1110","00"],
     ["1111","11"]
 ]
-t = table_verite()
-affTV(t)
-affK(t)
+t = [['a1', 'a0'], ['s'], ['00', '1'], ['01', '1'], ['10', '1'], ['11', '0']]
+t2= [['a3', 'a2', 'a1', 'a0'], ['s1', 's0'], ['0000', '11'], ['0001', '11'], ['0010', '11'], ['0011', '10'], ['0100', '01'], ['0101', '11'], ['0110', '01'], ['0111', '11'], ['1000', '11'], ['1001', '00'], ['1010', '10'], ['1011', '11'], ['1100', '11'], ['1101', '11'], ['1110', '11'], ['1111', '10']]
+
+#affTV(t)
+#affK(t)
+#affTVtk(t2)
+affKtk(t2)
